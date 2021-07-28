9f69f5a42f382eb1d98ae0caa1f5201b0c2e3017
missi0415 <missi0415@gmail.com>
2021-07-28 13:41:21 +0900

    020 関数 finish

---
+++ javascript-essence-tutorial/010_前提知識/020_関数/start/main.js
@@ -0,0 +1,11 @@
+let name = 'miura'
+function logoutput(na) {
+  console.log('helllo' + na);
+}
+logoutput(name);
+let ccc = "ukeru"
+hello(ccc, name);
+
+function hello(b,name){
+  console.log(name +"yeeeee" + b);
+}
