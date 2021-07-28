4a61c3b9b07ddd084e237b32771d62471bcd2e27
missi0415 <missi0415@gmail.com>
2021-07-28 20:00:21 +0900

    030 020 関数・ブロックスコープ

---
+++ javascript-essence-tutorial/030_スコープ/020_関数スコープとブロックスコープ/start/main.js
@@ -0,0 +1,24 @@
+function a() {
+  let b = 0;
+  //関数スコープこの中のこと
+  console.log(b)
+}
+a();
+
+//{}ブロックスコープとはこの波括弧のこと
+//function {}のことは関数スコープと呼び、ブロックスコープとは呼ばない
+{
+  var c = 1;
+  function d() {
+    console.log("d is called")
+  }
+  const e = function () {
+    console.log("ブロックスコープが働く宣言")
+  }
+  d();
+  e();
+}
+// var と関数宣言ではブロック宣言は無視される。
+// そのためブロックスコープ内で使用するのはlet,constもしくは変数宣言（let a = function()みたいなやつ）のみにしておく
+//ブロックスコープは普段はif(条件){ここがブロックスコープ}とかで使われている。
+console.log(c);
